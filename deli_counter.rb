27a23afce9991602a37dd78e89a36cec0326a661
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 13:22:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
 # Write your code here.
+
 def line(deli)
   if deli.empty?
     puts "The line is currently empty."
@@ -10,6 +11,9 @@ def line(deli)
   puts current_line
   end
 end
+
+def take_a_number(deli, name)
+  
 
   
 
