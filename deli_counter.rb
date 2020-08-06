98ac238b4c541a53bae6fc4e49e3a2fff20224e8
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 11:58:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli.length  == 0 
+  katz_deli  == 0 
     puts "The line is currently empty."
   else
     string = "The line is currently:"
