f5f5755321976add75ba7d5c38104acbb5059f48
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 12:06:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,10 @@
 # Write your code here.
 def line(katz_deli)
-  katz_deli.empty? "The line is currently empty."" : 
-    
-    string = "The line is currently:"
-    katz_deli.each_with_index do | customer, index |
-      string << "#{index +1}. #{customer}"
+  katz_deli.empty.length == 0 
+    "The line is currently empty."
   end
-  puts string
-  end
-end
+  
+   
 
 
   
