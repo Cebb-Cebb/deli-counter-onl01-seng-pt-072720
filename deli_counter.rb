38ad9d9fe5d8e2a9692ab7bfbcb636b61c48f629
hersson ceballos <herssonceballos@gmail.com>
2020-08-06 11:49:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,18 @@
 # Write your code here.
 def line(katz_deli)
-  katz_deli = []
-  if katz_deli.length  = 0 
+  if katz_deli.length  == 0 
     puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    katz_deli.each_with_index do | customer, index |
+      string << "#{index +1}. #{customer}"
   end
-  
+  puts string
+  end
 end
   
   
-  def ldef line(katz_deli)
-   if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-     string = "The line is currently: "
-    katz_deli.each_with_index do | customer, index |
-      string << "#{index +1}. #{customer} "
-    end
-    puts string
-  end
-endine(katz_deli)
+  def line(katz_deli)
    if katz_deli.length == 0
     puts "The line is currently empty."
   else
